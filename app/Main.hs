1c23da700fe71e014a27adad508a3cf64b68b257
cranej <crane@cranejin.com>
2016-08-15 16:14:51 +0800

    Output format accommodated to `sha1sum`'s output.

--- app/Main.hs
+++ app/Main.hs
@@ -1,22 +1,24 @@
 module Main where
 
-import qualified Data.ByteString.Lazy as LB
+import           Control.Monad.Trans.Maybe
+import qualified Data.ByteString.Lazy      as LB
 import           Lib
-import           System.Environment   (getArgs)
+import           System.Environment        (getArgs)
+import           System.FilePath           (takeFileName)
 
 usage = "hhash [-a {md5 | sha1 | sha256 | sha384 | sha512 | ripemd160}] path"
 defaultAlg = "sha256"
 
-hash :: [String] -> IO(Maybe String)
-hash ("-a":alg:path:_) = getHash alg <$> LB.readFile path
-hash ("-h":_) = return . Just $ usage
-hash ("--help":_) = return . Just $ usage
+hash :: [String] -> MaybeT IO String
+hash ("-a":alg:path:_) = (\x -> x ++ "  " ++ takeFileName path) <$> (MaybeT $ getHash alg <$> LB.readFile path)
+hash ("-h":_) = MaybeT $ return . Just $ usage
+hash ("--help":_) = MaybeT $ return . Just $ usage
 hash args = hash ("-a":defaultAlg:args)
 
 main :: IO ()
 main = do
     args <- getArgs
-    h <- hash args
+    h <- runMaybeT $ hash args
     case h of
       Just s -> putStrLn s
       Nothing -> putStrLn $ "Unsupported usage. \r\n" ++ usage
